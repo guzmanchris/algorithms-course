92a4948758306d042cf92aff27ad070d1540ef1d
Christian D. Guzmán Torres <christian.guzman1@upr.edu>
2021-05-14 10:15:55 -0400

    More descriptive variable names. Include spreadsheet for visualizing algorithm.

--- knapsack.py
+++ knapsack.py
@@ -4,15 +4,15 @@ import numpy as np
 def knapsack(v, w, W):
     # Initialize variables
     n = len(v)  # The amount of values we can fill knapsack with.
-    K = np.zeros((W + 1, n + 1), dtype=int)  # 2D array filled with zeros.
+    K = np.zeros((W + 1, n + 1), dtype=int)  # 2D array filled with zeros. Extra row and column to include weight 0
+    # and no values
 
     # Update the values in the K array
-    for i in range(1, n + 1):
-        for x in range(1, W + 1):
-            K[x][i] = K[x, i - 1]
-            if w[i - 1] <= x:
-                K[x][i] = max(K[x][i], K[x - w[i - 1]][i - 1] + v[i - 1])
-
+    for i in range(1, n + 1):  # i is the ith value available.
+        for weight in range(1, W + 1):
+            kvalue_without_weight = K[weight][i - 1]
+            kvalue_including_weight = K[weight - w[i - 1]][i - 1] + v[i - 1] if w[i - 1] <= weight else K[weight, i - 1]
+            K[weight][i] = max(kvalue_without_weight, kvalue_including_weight)
     return K[W][n]
 
 
