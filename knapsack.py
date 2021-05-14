2285921a1eb04a40a3cae1f808010e9af400c307
Christian D. Guzmán Torres <christian.guzman1@upr.edu>
2021-05-14 10:29:55 -0400

    [Documentation] Include more documentation in knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,7 +1,33 @@
+"""
+In the Knapsack problem, we are given a set of items, each with a weight and a value, and we need to determine the
+number of each item to include in a collection so that the total weight is less than or equal to a given limit and
+the total value is as large as possible.
+
+Input:
+
+value = [ 20, 5, 10, 40, 15, 25 ]
+weight = [ 1, 2, 3, 8, 7, 4 ]
+int W = 10
+
+Output: Knapsack value is 60
+
+value = 20 + 40 = 60
+weight = 1 + 8 = 9 < W
+
+
+"""
 import numpy as np
 
 
 def knapsack(v, w, W):
+    """
+    Calculates the maximum value that can be stored in a knapsack by storing the items of different values
+    and weights given in v, w, and with weight capacity W.
+    :param v: an iterable containing the values for each item.
+    :param w: an iterable containing the weights for each item.
+    :param W: the maximum capacity of the knapsack.
+    :return: The maximum value obtainable by storing a combination of the items.
+    """
     # Initialize variables
     n = len(v)  # The amount of values we can fill knapsack with.
     K = np.zeros((W + 1, n + 1), dtype=int)  # 2D array filled with zeros. Extra row and column to include weight 0
